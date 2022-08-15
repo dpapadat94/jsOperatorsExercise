f2061273d7a7d20ed18747ca6fb479f0768757e0
dpapadat94 <102779032+dpapadat94@users.noreply.github.com>
2022-08-15 12:23:08 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+console.log("EXERCISE 1:\n==========\n");
+// YOUR CODE HERE
+let a = 20;
+let b = 4;
+
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+// Exercise 2
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str);
+consle.log(num + str2);
+console.log(num + isPresent);
+console.log(isPresent + str);
+console.log(firstName + lastName);
+
+// Exercise 3
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(val == str3);
+console.log(val === str3);
+console.log(!isAwake);
+console.log("eleven" == str4 && val >= str3);
+console.log(!isAwake || isAwake);
+console.log(0 == falsae);
+console.log(0 === false);
+console.log(0 != flase);
+console.log(0 !== false);
